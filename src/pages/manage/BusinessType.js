85e8171d898cabc180e4f3b8257886295eefbe38
Ranjan <ranjanpandey2006@gmail.com>
2024-12-25 18:29:12 +0100

    industry and bizz type

--- src/pages/manage/BusinessType.js
+++ src/pages/manage/BusinessType.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
 import ManageMenu from '../../components/ManageMenu'
 import { useSelector } from "react-redux";
 import { Navigate } from 'react-router-dom';
+import crudService from '../../services/crud.service';
+import LoadingIcon from '../../components/LoadingIcon';
+import Swal from 'sweetalert2'
 
 const BusinessType = () => {
 
+    const [name, setName] = useState("");
+    const [id, setId] = useState("");
+    const [description, setDescription] = useState("");
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     const { user, isLoggedIn } = useSelector(state => state.auth);
+
+    const onSubmit = async (event)=>{
+            event.preventDefault();
+            let request = {name, description}
+            let response = null;
+            if(id){
+                response = await crudService.update(request, "businesses/"+id);
+            }else{
+                response = await crudService.create(request, "businesses");
+            }
+            if (response.status == 200 || response.status == 201) {
+                loadItems();
+                Swal.fire({
+                    title: 'Congratulations!',
+                    text: "Business type has been updated!",
+                    icon: "success"
+                  });
+              }
+        }
+    
+        const handleEdit = async (id) => {
+            try {
+                setLoading(true);
+                const resData = await crudService.getOne("businesses/"+id);
+                setName(resData.data.name);
+                setDescription(resData.data.description);
+                setId(resData.data.id);
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                console.error("Error fetching data:", error);
+            }
+        }
+    
+        const loadItems = async () => {
+            try {
+                setLoading(true);
+                const resData = await crudService.getAll("businesses");
+                setItems(resData.data);
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                console.error("Error fetching data:", error);
+            }
+        }
+    
+        useEffect(()=>{
+            loadItems();
+        }, []);
       
       if (!isLoggedIn) {
         return <Navigate to="/login" />;
@@ -18,15 +76,17 @@ const BusinessType = () => {
                         <div className="col-lg-12 col-md-12 col-sm-12">
                             <div className="card shadow-sm">
                                 <div className="card-body">
-                                    <h3 className="card-title text-center mb-4">Create Business Type</h3>
-                                    <form id="registrationForm">
+                                    { id ? <h3 className="card-title text-center mb-4">Edit Business Type</h3>
+                                     : <h3 className="card-title text-center mb-4">Create Business Type</h3>}
+                                    <form id="registrationForm" onSubmit={onSubmit}>
+                                        <input type="hidden" value={id} />
                                         <div className="mb-3">
-                                            <label htmlFor="bname" className="form-label">Business Name</label>
-                                            <input type="text" className="form-control" id="bname" placeholder="Enter business name" required />
+                                            <label htmlFor="bname" className="form-label">Business Type Name</label>
+                                            <input onChange={(e)=>setName(e.target.value)} value={name} type="text" className="form-control" id="bname" placeholder="Enter business type name" required />
                                         </div>
                                         <div className="mb-3">
-                                            <label htmlFor="bdescription" className="form-label">Business Description</label>
-                                            <textarea placeholder="Enter business description" className="form-control" id="bdescription"></textarea>
+                                            <label htmlFor="bdescription" className="form-label">Business Type Description</label>
+                                            <textarea onChange={(e)=>setDescription(e.target.value)} value={description} placeholder="Enter business type description" className="form-control" id="bdescription"></textarea>
                                         </div>
                                         <div className="text-center d-grid">
                                             <button type="submit" className="btn btn-warning">Submit</button>
@@ -37,6 +97,43 @@ const BusinessType = () => {
                         </div>
                     </div>
                 </div>
+            {loading ? <LoadingIcon /> :
+            <div className="card mt-3 border border-warning mb-2">
+                <div className="card-body">
+                    <h3 className="card-title text-center mb-2">Business types</h3>
+                    <div className="table-responsive">
+                        <table className="table  table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Updated At</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                   items.length != 0 ? items.map((item)=>{
+                                        return <tr key={item.id}>
+                                            <th className="fs-6 fw-normal">{item.name}</th>
+                                            <td className="fs-6">{item.description}</td>
+                                            <td className="fs-6">{item.updatedAt}</td>
+                                            <td className="d-flex">
+                                                <button type="button" className="btn btn-outline-warning text-dark fw-medium" onClick={()=>handleEdit(item.id)}>Edit</button>
+                                                {/* <button type="button" class="btn btn-outline-secondary">Deactivate</button> */}
+                                            </td>
+                                        </tr>
+                                    }) : <tr>
+                                            <td colSpan={4} className="text-center">No result found</td>
+                                        </tr>
+                                }
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            }
         </div>
     )
 }
--- src/pages/manage/IndustryType.js
+++ src/pages/manage/IndustryType.js
@@ -1,44 +1,141 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ManageMenu from '../../components/ManageMenu'
 import { useSelector } from "react-redux";
 import { Navigate } from 'react-router-dom';
+import crudService from '../../services/crud.service';
+import LoadingIcon from '../../components/LoadingIcon';
+import Swal from 'sweetalert2'
 
 const IndustryType = () => {
 
-  const { user, isLoggedIn } = useSelector(state => state.auth);
-  
-  if (!isLoggedIn) {
-    return <Navigate to="/login" />;
-  }
-  return (
-    <div className="smp-container mx-auto">
+    const [name, setName] = useState("");
+    const [id, setId] = useState("");
+    const [description, setDescription] = useState("");
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const { user, isLoggedIn } = useSelector(state => state.auth);
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        let request = { name, description }
+        let response = null;
+        if (id) {
+            response = await crudService.update(request, "industries/" + id);
+        } else {
+            response = await crudService.create(request, "industries");
+        }
+        if (response.status == 200 || response.status == 201) {
+            loadItems();
+            Swal.fire({
+                title: 'Congratulations!',
+                text: "Industry type has been updated!",
+                icon: "success"
+            });
+        }
+    }
+
+    const handleEdit = async (id) => {
+        try {
+            setLoading(true);
+            const resData = await crudService.getOne("industries/" + id);
+            setName(resData.data.name);
+            setDescription(resData.data.description);
+            setId(resData.data.id);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    const loadItems = async () => {
+        try {
+            setLoading(true);
+            const resData = await crudService.getAll("industries");
+            setItems(resData.data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    useEffect(() => {
+        loadItems();
+    }, []);
+
+    if (!isLoggedIn) {
+        return <Navigate to="/login" />;
+    }
+    return (
+        <div className="smp-container mx-auto">
             <ManageMenu />
-                <div className="mt-3 border border-warning" data-aos="fade-right">
-                    <div className="row justify-content-center">
-                        <div className="col-lg-12 col-md-12 col-sm-12">
-                            <div className="card shadow-sm">
-                                <div className="card-body">
-                                    <h3 className="card-title text-center mb-4">Create Industry Type</h3>
-                                    <form id="registrationForm">
-                                        <div className="mb-3">
-                                            <label htmlFor="bname" className="form-label">Industry Name</label>
-                                            <input type="text" className="form-control" id="bname" placeholder="Enter industry name" required />
-                                        </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="bdescription" className="form-label">Industry Description</label>
-                                            <textarea placeholder="Enter industry description" className="form-control" id="bdescription"></textarea>
-                                        </div>
-                                        <div className="text-center d-grid">
-                                            <button type="submit" className="btn btn-warning">Submit</button>
-                                        </div>
-                                    </form>
-                                </div>
+            <div className="mt-3 border border-warning" data-aos="fade-right">
+                <div className="row justify-content-center">
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                        <div className="card shadow-sm">
+                            <div className="card-body">
+                                {id ? <h3 className="card-title text-center mb-4">Edit Industry Type</h3>
+                                    : <h3 className="card-title text-center mb-4">Create Industry Type</h3>}
+                                <form id="registrationForm" onSubmit={onSubmit}>
+                                    <input type="hidden" value={id} />
+                                    <div className="mb-3">
+                                        <label htmlFor="bname" className="form-label">Industry Type Name</label>
+                                        <input onChange={(e) => setName(e.target.value)} value={name} type="text" className="form-control" id="bname" placeholder="Enter industry type name" required />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="bdescription" className="form-label">Industry Type Description</label>
+                                        <textarea onChange={(e) => setDescription(e.target.value)} value={description} placeholder="Enter industry type description" className="form-control" id="bdescription"></textarea>
+                                    </div>
+                                    <div className="text-center d-grid">
+                                        <button type="submit" className="btn btn-warning">Submit</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            {loading ? <LoadingIcon /> :
+                <div className="card mt-3 border border-warning mb-2">
+                    <div className="card-body">
+                        <h3 className="card-title text-center mb-2">Industry types</h3>
+                        <div className="table-responsive">
+                            <table className="table  table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Updated At</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        items.length != 0 ? items.map((item) => {
+                                            return <tr key={item.id}>
+                                                <th className="fs-6 fw-normal">{item.name}</th>
+                                                <td className="fs-6">{item.description}</td>
+                                                <td className="fs-6">{item.updatedAt}</td>
+                                                <td className="d-flex">
+                                                    <button type="button" className="btn btn-outline-warning text-dark fw-medium" onClick={() => handleEdit(item.id)}>Edit</button>
+                                                    {/* <button type="button" class="btn btn-outline-secondary">Deactivate</button> */}
+                                                </td>
+                                            </tr>
+                                        }) : <tr>
+                                            <td colSpan={4} className="text-center">No result found</td>
+                                        </tr>
+                                    }
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
-  )
+    )
 }
 
 export default IndustryType
